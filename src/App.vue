72c1ce116c49acece5a659f06c7c600f4f526e8c
er3n <erenozturk@gmail.com>
2018-02-27 20:04:54 +0300

    scss

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="app">
+    <contact-header />
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
@@ -8,25 +9,18 @@
   </div>
 </template>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+  import ContactHeader from './components/ContactHeader'
 
-#nav {
-  padding: 30px;
-}
+  export  default {
+    components:  {
+      ContactHeader
+    }
+  }
+</script>
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
 
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
+<style lang="scss">
+  @import './styles/custom-bootstrap.scss';
+  @import '../node_modules/bootstrap/scss/bootstrap.scss';
 </style>
---
+++ src/components/ContactHeader.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        ContactHeader
+    </div>
+</template>
+
+<script>
+  export default {
+    name: 'ContactHeader'
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss">
+
+</style>

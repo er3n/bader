aa931966c70e65563054e1c89952ef9561eb1ce5
er3n <erenozturk@gmail.com>
2018-03-01 21:25:44 +0300

    üst menü gelir

---
+++ src/components/HomeSlider.vue
@@ -0,0 +1,51 @@
+<template>
+    <div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
+        <div class="carousel-inner" role="listbox">
+            <div class="carousel-item active">
+                <img class="d-block" src="../assets/handshake.jpg" alt="First slide">
+                    <div class="carousel-caption d-none d-md-block carousel-item-left" id="wrapper">
+                    <h1 class="font-weight-bold">Arabuluculuk</h1>
+
+                    <h1 class="font-weight-bold" style="font-size: 50px">Ekenomik, Hızlı, Gizli ve Gönüllü Çözüm</h1>
+                    <p>Hukuk uyuşmazlıklarında arabulucuk faaliyetlerini tanıtmak yaygınlaştırmak, faaliyetlere ilişkin yönetim ve standardı belirlemek.</p>
+                    <button type="button" class="btn aboutusbutton">Derneğimiz</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+  export default {
+    name: 'HomeSlider'
+
+  }
+</script>
+
+<style lang="scss" scoped>
+
+    $basecolor: #276BA6;
+
+    .carousel-caption {
+        top: auto;
+        bottom: 20px;
+        right: auto;
+    }
+
+    #wrapper {
+        width: 500px;
+        margin: 0 auto;
+        text-align: left;
+    }
+
+    .aboutusbutton {
+        background-color: $basecolor !important;
+        color: white;
+        border-radius: 0;
+
+        &:hover {
+            background-color: lighten($basecolor, 30) !important;
+        }
+    }
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,19 @@
 <template>
-  <div class="home">
-    Home
+  <div>
+    <home-slider />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
+import HomeSlider from '../components/HomeSlider.vue'
 
 export default {
   name: 'home',
   components: {
-    HelloWorld
+    HelloWorld,
+    HomeSlider
   }
 }
 </script>

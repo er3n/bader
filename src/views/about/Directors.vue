edcf74239ba4fff1d5ba00d1679c4d0528899599
er3n <erenozturk@gmail.com>
2018-03-10 18:30:29 +0300

    resimler düzenlenir

--- src/views/about/Directors.vue
+++ src/views/about/Directors.vue
@@ -2,5 +2,99 @@
     <div class="col-12 col-md-9">
         <h2>Yönetim Kurulu</h2>
         <hr>
+
+<div class="row">
+    <div class="col-12 col-md-4">
+        <div class="card">
+            <img class="card-img-bottom" src="../../assets/dy.jpg" alt="Card image cap">
+            <div class="card-body mx-auto">
+                <h5 class="card-title">Dursun Yassikaya</h5>
+                <hr>
+                
+                <ul class="list-group">
+                    <li class="list-group-item">
+                        Başkan
+                    </li>
+                    <li class="list-group-item">
+                        Ankara Ünv. Hukuk Fak.
+                    </li>
+                    <li class="list-group-item">
+                        Avukat - Arabulucu
+                    </li>
+                    <li class="list-group-item">
+                        Arb. Sicil No: 1211
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+
+    <div class="col-12 col-md-4">
+        <div class="card">
+            <img class="card-img-bottom" src="../../assets/eda.jpg" alt="Card image cap">
+            <div class="card-body mx-auto">
+                <h5 class="card-title">A. Eda Günday</h5>
+                <hr>
+                
+                <ul class="list-group">
+                    <li class="list-group-item">
+                        Genel Sekreter
+                    </li>
+                    <li class="list-group-item">
+                        Ankara Ünv. Hukuk Fak.
+                    </li>
+                    <li class="list-group-item">
+                        Avukat - Arabulucu
+                    </li>
+                    <li class="list-group-item">
+                        Arb. Sicil No: 2942
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+
+    <div class="col-12 col-md-4">
+        <div class="card">
+            <img class="card-img-bottom" src="../../assets/mehmet.jpg" alt="Card image cap" >
+            <div class="card-body mx-auto">
+                <h5 class="card-title">Mehmet Erşahin</h5>
+                <hr>
+                
+                <ul class="list-group">
+                    <li class="list-group-item">
+                        Başkan Yardımcısı
+                    </li>
+                    <li class="list-group-item">
+                        Ankara Ünv. Hukuk Fak.
+                    </li>
+                    <li class="list-group-item">
+                        Avukat - Arabulucu
+                    </li>
+                    <li class="list-group-item">
+                        Arb. Sicil No: 1782
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+
+</div>
+
     </div>
 </template>
+
+
+<style lang="scss" scoped>
+
+$basecolor: #276ba6;
+
+.card {
+  -webkit-box-shadow: 0 0px 12px -6px #999;
+  -moz-box-shadow: 0 8px 12px -6px #999;
+  box-shadow: 0 0px 12px 0px #999;
+  border: 0;
+  background-color: $basecolor;
+  color: white;
+}
+</style>

5e5eaf9a1fb4d3c7dbfd34335255b420f59cf1b3
er3n <erenozturk@gmail.com>
2018-03-26 21:58:50 +0300

    responsive olur

--- src/components/ImageGalery.vue
+++ src/components/ImageGalery.vue
@@ -2,7 +2,7 @@
   <div class="col-12">
      <gallery :images="images" :index="index" @close="index = null"></gallery>
      <div class="row">
-      <div class="col-md-4" v-for="(image, imageIndex) in images" :key=imageIndex @click="index = imageIndex" >
+      <div class="col-12 col-md-4" v-for="(image, imageIndex) in images" :key=imageIndex @click="index = imageIndex" >
         <img v-lazy="image" class="img-fluid mt-4" />
       </div>
      </div>
@@ -13,7 +13,7 @@
 import VueGallery from "vue-gallery";
 
 export default {
-    props: ['images'],
+  props: ["images"],
   data: function() {
     return {
       index: null

46b6f317da7b7445f598203aa61ca311ba0387d0
er3n <erenozturk@gmail.com>
2018-04-03 00:36:35 +0300

    resimler küçültülür

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,30 @@
 var PrerenderSpaPlugin = require('prerender-spa-plugin')
 var path = require('path')
 
+const prerenderPages = [
+  '/hakkimizda/amac-ilke-deger',
+  '/hakkimizda/yonetim-kurulu',
+  '/hakkimizda/denetim-kurulu',
+  '/hakkimizda/kurucular',
+  '/hakkimizda/tuzugumuz',
+  '/arabuluculuk/arabuluculuk-nedir',
+  '/arabuluculuk/sss',
+  '/arabuluculuk/arabuluculuk-kitapligi',
+  '/mevzuat/ucretlendirme',
+  '/uyelik/kosullar',
+  '/uyelik/banka-bilgileri',
+  '/faaliyetlerimiz/ziyaret',
+  '/faaliyetlerimiz/etkinlik',
+  '/faaliyetlerimiz/makaleler',
+  '/faaliyetlerimiz/egitim-sunumlari',
+  '/basin/tv-programlari',
+  '/basin/radyo-programlari',
+  '/basin/yazili-basin',
+  '/iletisim',
+  '/duyurular'
+]
+
+
 module.exports = {
   lintOnSave: false,
   configureWebpack: config => {
@@ -12,7 +36,7 @@ module.exports = {
           // Absolute path to compiled SPA
           path.resolve(__dirname, 'dist'),
           // List of routes to prerender
-          [ '/', '/about', '/portfolio/one', '/portfolio/two', '/portfolio/three', ],
+          prerenderPages,
           {
               // options
           }

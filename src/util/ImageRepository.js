d0bedbc7b63345ea88ff32559903c4a31259a36f
er3n <erenozturk@gmail.com>
2018-11-15 23:21:59 +0300

    bader prefix removed from folder names

--- src/util/ImageRepository.js
+++ src/util/ImageRepository.js
@@ -2,6 +2,6 @@ import axios from 'axios'
 
 export default function(folder) {
     const requestUrl =
-    `https://bader-server.herokuapp.com/image-list?folder=bader/${folder}`;
+    `https://bader-server.herokuapp.com/image-list?folder=${folder}`;
     return axios.get(requestUrl)
 }

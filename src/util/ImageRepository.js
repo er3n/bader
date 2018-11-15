1226db3a00832c43ef59b1543c8bce820e495f2b
er3n <erenozturk@gmail.com>
2018-11-15 22:51:03 +0300

    images loaded from heroku

--- src/util/ImageRepository.js
+++ src/util/ImageRepository.js
@@ -2,6 +2,6 @@ import axios from 'axios'
 
 export default function(folder) {
     const requestUrl =
-    `http://localhost:3000/image-list?folder=bader/${folder}`;
+    `https://bader-server.herokuapp.com/image-list?folder=bader/${folder}`;
     return axios.get(requestUrl)
 }
